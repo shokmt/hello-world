2c8a721eb1726499e9a980b22425dc982a0ba0bb
Shusuke Okamoto <s.okamoto@computer.org>
2016-05-13 11:45:26 +0900

    Add a C program of hello-world

---
+++ hello.c
@@ -0,0 +1,10 @@
+/**************************************\
+|  hello, world                        |
+\**************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   printf("hello, world\n");
+}
+
